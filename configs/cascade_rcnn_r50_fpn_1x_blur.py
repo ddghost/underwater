8d674ee3691b9e5dad3a9e96891b7164874f5598
ddghost <544011312@qq.com>
2020-03-16 19:54:24 +0800

    update

--- configs/cascade_rcnn_r50_fpn_1x_blur.py
+++ configs/cascade_rcnn_r50_fpn_1x_blur.py
@@ -174,7 +174,7 @@ albu_train_transforms = [
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=[(4096, 800), (4096, 1200)], keep_ratio=True),
+    dict(type='Resize', img_scale=[(4096, 800), (4096, 1200)],multiscale_mode='range', keep_ratio=True),
     dict(type='Pad', size_divisor=32),
     dict(type='RandomFlip', flip_ratio=0.5),
 
