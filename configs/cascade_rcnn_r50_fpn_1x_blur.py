7e761843941db05288e884393645d02338df3f5e
ddghost <544011312@qq.com>
2020-03-16 20:00:38 +0800

    update

--- configs/cascade_rcnn_r50_fpn_1x_blur.py
+++ configs/cascade_rcnn_r50_fpn_1x_blur.py
@@ -205,17 +205,7 @@ train_pipeline = [
 ]
 '''
 train_pipeline = [
-'''
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=[(4096, 800), (4096, 1200)],
-         multiscale_mode='range', keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-'''
+
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=[(4096, 800), (4096, 1200)],multiscale_mode='range', keep_ratio=True),
