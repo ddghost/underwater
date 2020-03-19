75bfd067d7d96f496b5f793586676356e968fa7e
ddghost <544011312@qq.com>
2020-03-19 10:30:03 +0800

    update

--- configs/cascade_rcnn_hrnetv2p_w48_2x_multiTest.py
+++ configs/cascade_rcnn_hrnetv2p_w48_2x_multiTest.py
@@ -119,12 +119,12 @@ data_root = 'data/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
-imgScale = 0.85
+imgScale = 0.83#!!!!!!
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize',  img_scale=( int(4096 * imgScale), int(1200 * imgScale)), keep_ratio=True),
-
+    dict(type='Resize', img_scale=[( int(4096 * imgScale), int(800 * imgScale) ), ( int(4096 * imgScale), int(1200 * imgScale))],
+         multiscale_mode='range', keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
