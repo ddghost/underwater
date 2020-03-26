18c01e1758d84a1da72d00575955a3f80c229367
ddghost <544011312@qq.com>
2020-03-27 00:35:19 +0800

    update

--- configs/cascade_htc_x101_reTrain_multiTest_0327_blur.py
+++ configs/cascade_htc_x101_reTrain_multiTest_0327_blur.py
@@ -193,13 +193,16 @@ train_pipeline = [
         },
         update_pad_shape=False,
         skip_img_without_anno=True),
-    #dict(type='Normalize', **img_norm_cfg),
+    dict(type='Collect', keys=['img']),
+    '''
+    dict(type='Normalize', **img_norm_cfg),
     dict(type='DefaultFormatBundle'),
     dict(
         type='Collect',
         keys=['img', 'gt_bboxes', 'gt_labels'],
         meta_keys=('filename', 'ori_shape', 'img_shape', 'img_norm_cfg',
                    'pad_shape', 'scale_factor'))
+    '''
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
