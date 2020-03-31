fedd723d5eb2584e186643e97c2b9ed64667b85d
ddghost <544011312@qq.com>
2020-03-31 18:56:55 +0800

    update

--- tools/test.py
+++ tools/test.py
@@ -130,7 +130,7 @@ def main():
     dataset = build_dataset(cfg.data.test)
     data_loader = build_dataloader(
         dataset,
-        imgs_per_gpu=1,
+        imgs_per_gpu=cfg.data.imgs_per_gpu*2,
         workers_per_gpu=cfg.data.workers_per_gpu,
         dist=distributed,
         shuffle=False)
