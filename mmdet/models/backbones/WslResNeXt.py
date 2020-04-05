b0fda0f56e52c65faa41072cb84e0acbef36d3f0
ddghost <544011312@qq.com>
2020-04-05 19:20:49 +0800

    update

--- mmdet/models/backbones/WslResNeXt.py
+++ mmdet/models/backbones/WslResNeXt.py
@@ -7,7 +7,10 @@ class WslResNeXt(nn.Module):
 	def __init__(self, **kwargs):
 		super(WslResNeXt, self).__init__()
 		self.model = torch.hub.load( 'facebookresearch/WSL-Images', 'resnext101_32x8d_wsl')
-	
+		self.norm_eval = True
+		self.frozen_stages = -1
+		
+		
 	def forward(self,x):
 		x = self.model.conv1(x)
 		x = self.model.norm1(x)
@@ -23,5 +26,31 @@ class WslResNeXt(nn.Module):
 		outs.append(x)
 		x = self.model.layer4(x)
 		outs.append(x)
+		
 
 		return tuple(outs)
+	def init_weights(self, pretrained=None):
+		pass
+	
+	def _freeze_stages(self):
+		if self.frozen_stages >= 0:
+			self.model.norm1.eval()
+			for m in [self.model.conv1, self.model.norm1]:
+				for param in m.parameters():
+					param.requires_grad = False
+
+		for i in range(1, self.frozen_stages + 1):
+			m = getattr(self.model, 'layer{}'.format(i))
+			m.eval()
+			for param in m.parameters():
+				param.requires_grad = False
+
+	def train(self, mode=True):
+		super(WslResNeXt, self).train(mode)
+		self._freeze_stages()
+		if mode and self.norm_eval:
+			for m in self.model.modules():
+				# trick: eval have effect on BatchNorm only
+				if isinstance(m, _BatchNorm):
+					m.eval()
+	
