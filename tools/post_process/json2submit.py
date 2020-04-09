559d3c466598f7ecc7eca6b0c72ac1e926c40a2a
ddghost <544011312@qq.com>
2020-04-09 09:34:06 +0800

    update

--- tools/post_process/json2submit.py
+++ tools/post_process/json2submit.py
@@ -14,7 +14,7 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    test_json_raw = json.load(args.ori_json, "r"))
+    test_json_raw = json.load(open(args.ori_json, "r"))
     test_json = json.load(open("results/" + args.test_json, "r"))
     submit_file_name = args.submit_file
     submit_path = 'submit/'
